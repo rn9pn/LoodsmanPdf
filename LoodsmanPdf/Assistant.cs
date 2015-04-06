7bb7294e383bea18b24fb1af0580811cd6d27807
klassen <rony9pony@gmail.com>
2015-04-06 18:58:19 +0300

    Add Assistant.GetAllDocs()

---
+++ LoodsmanPdf/Assistant.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Loodsman;
+using DataProvider;
+
+namespace LoodsmanPdf
+{
+    internal static class Assistant
+    {
+        private static object returncode = 0;
+        private static object errmes = 0;
+
+        /// <summary>
+        /// Возвращает id-шники всех объектов типа "Документ"
+        /// </summary>
+        /// <param name="_idVersion">Контекст поиска, задается через запятую</param>        
+        internal static List<int> GetAllDocs(string _idVersion)
+        {
+            List<int> allDocs = new List<int>();
+
+            DataSet docs = new DataSet();
+                        
+            docs.DATA = Main.APlugin.RunMethod("FindObjectsInContext2", new object[] { 
+                _idVersion,
+                "<FIND><LinksStep id=\"1\" stepname=\"Связи1\" direction=\"down\" recursive=\"true\"><LinkList><LinkType name=\"Журнал изменений\"/><LinkType name=\"Документы\"/>"+
+                "<LinkType name=\"На основании\"/><LinkType name=\"Разработан по\"/><LinkType name=\"Заявка на\"/><LinkType name=\"Использует\"/><LinkType name=\"Элемент маршрута\"/>"+
+                "<LinkType name=\"Касается\"/><LinkType name=\"Состоит из ...\"/><LinkType name=\"Содержит\"/><LinkType name=\"Для изделий\"/><LinkType name=\"Применяется для ...\"/>"+
+                "<LinkType name=\"Исполнения\"/><LinkType name=\"Изготавливается из ...\"/><LinkType name=\"Заготовка для\"/><LinkType name=\"Маршрут\"/>"+
+                "<LinkType name=\"Создает версию ...\"/><LinkType name=\"Оснастка\"/><LinkType name=\"Проектная документация\"/><LinkType name=\"Потребляет\"/>"+
+                "<LinkType name=\"Взаимозаменяемые\"/><LinkType name=\"Фрагменты\"/><LinkType name=\"Документы в архив\"/><LinkType name=\"Приложение\"/><LinkType name=\"Эквивалент\"/>"+
+                "<LinkType name=\"Технологическая сборка\"/><LinkType name=\"Для операции\"/><LinkType name=\"Для сборки\"/><LinkType name=\"Технологический состав для\"/>"+
+                "</LinkList><FilterStep id=\"2\" stepname=\"Фильтр1\"><TypeList><Type name=\"Документ\"/></TypeList><ResultStep id=\"3\" stepname=\"Результат1\" AllResult=\"union\"/>"+
+                "</FilterStep></LinksStep></FIND>",
+                0,
+                returncode,
+                errmes
+            });
+
+            if (docs.RecordCount > 0)
+            {
+                docs.First();
+                while (!docs.Eof)
+                {
+                    if (docs.get_FieldValue("_TYPE") == "Сборочная единица")
+                    {
+                        allDocs.Add(docs.get_FieldValue("_ID_VERSION"));
+                    }
+                    docs.Next();
+                }
+            }
+
+            return allDocs;
+        }
+    }
+}
--- LoodsmanPdf/Main.cs
+++ LoodsmanPdf/Main.cs
@@ -7,6 +7,7 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using Loodsman;
 using DataProvider;
+using NLog;
 
 namespace LoodsmanPdf
 {
@@ -24,9 +25,58 @@ namespace LoodsmanPdf
     [ComVisible(true)]
     public class Main : ILoodsmanPdf
     {
-        public void Command1(IPluginCall APlugin)
+        public static IPluginCall APlugin;
+        public static Logger logger;        
+        private object returncode = 0;
+        private object errmes = 0;
+
+        public Main()
         {
-            MessageBox.Show("Hi!");
+            logger = LogManager.GetCurrentClassLogger();
+            string pathNlogConfig = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\Nlog.config";
+            if (File.Exists(pathNlogConfig))
+            {
+                LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration(pathNlogConfig, true);
+            }
+            else
+            {
+                MessageBox.Show("Не найден файл \"Nlog.config\" по следующему пути: " + pathNlogConfig);
+                return;
+            }
+        }
+
+        public void Command1(IPluginCall _APlugin)
+        {
+            logger.Info("Создать вторичное представление");
+
+            APlugin = _APlugin;
+
+            switch(APlugin.stType)
+            {
+                case "Сборочная единица":
+                    {
+                        logger.Info("Выбран тип \"Сборочная единица\"");
+
+                        List<int> allDocs = Assistant.GetAllDocs(Convert.ToString(APlugin.IdVersion));
+                    }
+                    break;
+
+                case "Документ":
+                    {
+                        logger.Info("Выбран тип \"Документ\"");
+                    }
+                    break;
+
+                default:
+                    {
+                        logger.Info("Выбран тип \"" + APlugin.stType + "\"");
+
+                        MessageBox.Show("Выбирите тип \"Сборочная единица\" или \"Документ\"");
+                    }
+                    break;
+            }
+
+            logger.Info("Выход из программы");
         }
     }
 }
